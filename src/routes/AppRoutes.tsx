23a77f84fa06e7fb6192b109c459a6c6a214b501
Zaira <zairahussain27@gmail.com>
2026-06-25 00:08:35 +0530

    Zaira feat: add AI-focused admin navigation with custom dashboard modules

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import { LandingPage } from '../pages/LandingPage';
-import { StudentDashboard } from '../pages/StudentDashboard';
+import { AdminDashboard } from '../pages/AdminDashboard';
 
 export const AppRoutes: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/StudentDashboard" element={<StudentDashboard />} />
+        <Route path="/admin-dashboard" element={<AdminDashboard />} />
       </Routes>
     </BrowserRouter>
   );
