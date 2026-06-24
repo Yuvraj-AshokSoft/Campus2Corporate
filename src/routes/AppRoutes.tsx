1c1a893ca7d6ddc6b3cdc061b2ea9445da0053b3
khushboo walwani <walwanikhushboo@gmail.com>
2026-06-25 00:55:28 +0530

    khushboo

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -3,13 +3,15 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import { LandingPage } from '../pages/LandingPage';
 import { AdminDashboard } from '../pages/AdminDashboard';
+import { StudentDashboard } from '../pages/StudentDashboard';
 
 export const AppRoutes: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/admin-dashboard" element={<AdminDashboard />} />
+        <Route path="/admindashboard" element={<AdminDashboard />} />
+        <Route path="/studentdashboard" element={<StudentDashboard />} />
       </Routes>
     </BrowserRouter>
   );
