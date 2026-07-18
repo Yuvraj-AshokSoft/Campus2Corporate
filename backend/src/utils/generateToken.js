b8539a51d64c6666f8b3d29eb7b7c9efbdabb643
Zaira <zairahussain27@gmail.com>
2026-07-19 00:40:36 +0530

    zaira :resolved merge conflict

--- backend/src/utils/generateToken.js
+++ backend/src/utils/generateToken.js
@@ -1,12 +1,15 @@
 import jwt from "jsonwebtoken";
 
-const generateToken = (id) => {
+const generateToken = (student) => {
+  const secret = process.env.JWT_SECRET || "fallback_jwt_secret_key_123456";
+
   return jwt.sign(
-    { id },
-    process.env.JWT_SECRET,
     {
-      expiresIn: "7d",
-    }
+      id: student._id.toString(),
+      role: "student",
+    },
+    secret,
+    { expiresIn: process.env.JWT_EXPIRES_IN || "30d" }
   );
 };
 
